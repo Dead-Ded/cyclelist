acf8457ddf4a55d71efc3d36cdbe79e8347a3e81
Dead-Ded <musailov2004@mail.ru>
2021-11-28 19:06:26 +0400

    1-st commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='looplist',
+      version='0.0',
+      description='Adds looped containers to Python 3.x',
+      packages=['looplist'],
+      author_email='musailov2004@mail.ru',
+      zip_safe=False)
